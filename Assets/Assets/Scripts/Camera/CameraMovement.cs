95971a8724ebf5715013cb0d91f1bae4b415dabd
Huseyin Orhan <huseyinsoftware@gmail.com>
2024-09-29 22:25:33 +0300

    Camera Control

---
+++ Assets/Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 10.0f;
+    [SerializeField] private float rotSpeed = 10.0f;
+    [SerializeField] private float zoomSpeed = 10.0f;
+
+    [SerializeField] private float minXRotaion = 20;
+    [SerializeField] private float maxXRotaion = 90;
+
+    private Vector3 dragOrigin;
+
+    [SerializeField] private CinemachineFreeLook virtualCamera;
+    [SerializeField] private Camera mainCamera;
+
+    void Start()
+    {
+        CinemachineCore.GetInputAxis = GetAxisCustom;
+    }
+
+    public float GetAxisCustom(string axisName)
+    {
+        if (axisName == "Mouse X")
+        {
+            if (Input.GetMouseButton(1))
+            {
+                return -UnityEngine.Input.GetAxis("Mouse X");
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        else if (axisName == "Mouse Y")
+        {
+            return -UnityEngine.Input.GetAxis("Mouse ScrollWheel") * (1/Time.deltaTime) * zoomSpeed;
+        }
+
+        return UnityEngine.Input.GetAxis(axisName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveDirection = new Vector3(0, 0, 0);
+
+        // Get the forward and right vectors relative to the camera's orientation
+        Vector3 cameraForward = mainCamera.transform.forward;
+        Vector3 cameraRight = mainCamera.transform.right;
+
+        // Flatten the forward and right vectors so the movement stays horizontal
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+
+        // Handle movement input
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveDirection += cameraForward;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveDirection -= cameraForward;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveDirection -= cameraRight;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveDirection += cameraRight;
+        }
+
+        // Normalize the direction and move the object
+        moveDirection.Normalize();
+        transform.Translate(moveDirection * movementSpeed * Time.deltaTime, Space.World);
+    }
+}
--- Assets/Easy Build System/Packages/Addons/House Buildings/Scripts/Demo_OrbitCamera.cs
+++ Assets/Easy Build System/Packages/Addons/House Buildings/Scripts/Demo_OrbitCamera.cs
@@ -1,14 +1,5 @@
-﻿/// <summary>
-/// Project : Easy Build System
-/// Class : Demo_OrbitCamera.cs
-/// Namespace : EasyBuildSystem.Packages.Addons.BuildingMenu
-/// Copyright : © 2015 - 2022 by PolarInteractive
-/// </summary>
-
+﻿
 using UnityEngine;
-#if EBS_INPUT_SYSTEM_SUPPORT
-using UnityEngine.InputSystem;
-#endif
 
 namespace EasyBuildSystem.Packages.Addons.BuildingMenu
 {
@@ -27,58 +18,6 @@ namespace EasyBuildSystem.Packages.Addons.BuildingMenu
         {
             m_PanMovement = Vector3.zero;
 
-#if EBS_INPUT_SYSTEM_SUPPORT
-
-            if (Keyboard.current.wKey.isPressed)
-            {
-                m_PanMovement += Vector3.forward * m_MovementSpeed * Time.deltaTime;
-            }
-
-            if (Keyboard.current.sKey.isPressed)
-            {
-                m_PanMovement -= Vector3.forward * m_MovementSpeed * Time.deltaTime;
-            }
-
-            if (Keyboard.current.aKey.isPressed)
-            {
-                m_PanMovement += Vector3.left * m_MovementSpeed * Time.deltaTime;
-            }
-
-            if (Keyboard.current.dKey.isPressed)
-            {
-                m_PanMovement += Vector3.right * m_MovementSpeed * Time.deltaTime;
-            }
-
-            transform.Translate(transform.TransformDirection(m_PanMovement), Space.World);
-
-            Vector2 inputAxis = Mouse.current.position.ReadValue();
-
-            if (Mouse.current.rightButton.isPressed)
-            {
-                Vector3 mouseDelta;
-
-                if (m_LastMousePosition.x >= 0 &&
-                    m_LastMousePosition.y >= 0 &&
-                    m_LastMousePosition.x <= Screen.width &&
-                    m_LastMousePosition.y <= Screen.height)
-                    mouseDelta = new Vector3(inputAxis.x, inputAxis.y, 0) - m_LastMousePosition;
-                else
-                {
-                    mouseDelta = Vector3.zero;
-                }
-
-                Vector3 rotation = Vector3.up * Time.deltaTime * m_RotateSpeed * mouseDelta.x;
-
-                transform.Rotate(rotation, Space.World);
-
-                rotation = transform.rotation.eulerAngles;
-                rotation.z = 0;
-
-                transform.rotation = Quaternion.Euler(rotation);
-            }
-
-            m_LastMousePosition = new Vector3(inputAxis.x, inputAxis.y, 0);
-#else
             if (Input.GetKey(KeyCode.W))
             {
                 m_PanMovement += Vector3.forward * m_MovementSpeed * Time.deltaTime;
@@ -126,7 +65,6 @@ namespace EasyBuildSystem.Packages.Addons.BuildingMenu
             }
 
             m_LastMousePosition = Input.mousePosition;
-#endif
         }
     }
 }
