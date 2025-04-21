ae4c1494c0326c298b5eae0c3a587e72ea82e288
Huseyin Orhan <huseyinsoftware@gmail.com>
2025-04-21 11:42:05 +0300

    Project Version Update

--- Assets/Easy Build System/Packages/Addons/Buggy Constructor/Scripts/Demo_BuildingDragging.cs
+++ Assets/Easy Build System/Packages/Addons/Buggy Constructor/Scripts/Demo_BuildingDragging.cs
@@ -142,7 +142,7 @@ public class Demo_BuildingDragging : MonoBehaviour
             Vector3 mousePositionOffset =
                 Camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, m_SelectionDistance)) - m_OriginalScreenTargetPosition;
 
-            m_DraggingObject.velocity = (m_OriginalRigidbodyPosition + mousePositionOffset - m_DraggingObject.transform.position) * m_DraggingForce * Time.deltaTime;
+            m_DraggingObject.linearVelocity = (m_OriginalRigidbodyPosition + mousePositionOffset - m_DraggingObject.transform.position) * m_DraggingForce * Time.deltaTime;
 #endif
         }
     }
